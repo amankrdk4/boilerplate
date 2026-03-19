a1dc189d5ee51d1900daca760e293a6ecc95228e
alpha <alpha>
2026-03-19 10:53:27 +0530

    usaco custom template added

---
+++ usaco.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*--------------------------------------------------------------------------------------------------------------------------*/
+/** * MACROS & TYPEDEFS 
+ **/
+#define int           long long  // Global int as long long
+#define fastio()      ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define nline         "\n"
+#define pb            push_back
+#define ppb           pop_back
+#define mp            make_pair
+#define ff            first
+#define ss            second
+#define sz(x)         ((int)(x).size())
+#define all(x)        (x).begin(), (x).end()
+
+typedef unsigned long long ull;
+typedef long double lld;
+
+/** * CONSTANTS **/
+const int MOD  = 1e9 + 7;
+const int MOD1 = 998244353;
+const int INF  = 1e18;
+const lld PI   = 3.14159265358979323846;
+
+/**
+ * ENHANCED DEBUGGING SUITE
+ **/
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t)    {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t)   {cerr << t;}
+void _print(lld t)    {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t)    {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+// Forward Declaration for Bottom-Level Solve
+void solve();
+
+/**
+ * USACO & I/O HANDLER
+ **/
+void setIO(string name = "") {
+    fastio();
+    // High precision for AtCoder/USACO geometry/floating-point tasks
+    cout << fixed << setprecision(15); 
+#ifndef ONLINE_JUDGE
+    freopen("err.txt", "w", stderr);
+#endif
+#ifdef ONLINE_JUDGE
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+#endif
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int32_t main() {
+    /** * CONFIGURATION:
+     * 1. USACO: setIO("problemname"); and keep t = 1;
+     * 2. CF/AtCoder: setIO(""); and uncomment cin >> t;
+     **/
+    setIO("closing");
+
+    int t = 1;
+    /* cin >> t;  */
+    
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------------*/
+/**
+ * CORE LOGIC
+ **/
+/*--------------------------------------------------------------------------------------------------------------------------*/
+/**
+ * CORE LOGIC: USACO Silver - Closing the Farm
+ **/
+
+
+void solve() {
+}
+
