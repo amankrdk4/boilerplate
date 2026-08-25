af8233182f5783c5e8aaae74994708a4d494c271
alpha <alpha@123.com>
2026-08-25 20:47:11 +0530

    removed more arithmetic functions

--- alpha.cpp
+++ alpha.cpp
@@ -88,22 +88,7 @@ template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i
 /*--------------------------------------------------------------------------------------------------------------------------*/
 /** * IMPORTANT FUNCTIONS FOR CP **/
 
-template<typename T>
-T invpower(T base, T exp) {
-    T res = 1;
-    base %= MOD;
-    while (exp > 0) {
-        if (exp % 2 == 1) res = (res * base) % MOD;
-        base = (base * base) % MOD;
-        exp /= 2;
-    }
-    return res;
-}
 
-template<typename T>
-T modinv(T n) {
-    return invpower(n, (T)MOD - 2);
-}
 
 /* Modular Arithmetic Helpers */
 template<typename T> T modadd(T a, T b) { return ((a % MOD) + (b % MOD)) % MOD; }
@@ -112,25 +97,6 @@ template<typename T> T modmul(T a, T b) { return ((a % MOD) * (b % MOD)) % MOD; 
 template<typename T> T moddiv(T a, T b) { return modmul(a, modinv(b)); }
 
 
-/** * COMBINATORICS & FACTORIALS **/
-const int MAXN = 1e6 + 5;
-int fact[MAXN], invFact[MAXN];
-bool is_precomputed = false;
-
-
-template<typename T>
-T nCr(T n, T r) {
-    if (r < 0 || r > n) return 0;
-    if (!is_precomputed) precompute();
-    return modmul((T)fact[n], modmul((T)invFact[r], (T)invFact[n - r]));
-}
-
-template<typename T>
-T nPr(T n, T r) {
-    if (r < 0 || r > n) return 0;
-    if (!is_precomputed) precompute();
-    return modmul((T)fact[n], (T)invFact[n - r]);
-}
 
 void solve();
 
